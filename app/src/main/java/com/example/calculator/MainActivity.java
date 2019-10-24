21b94005c2b1e396456c9472f88d6394036c1dbf
승현 신 <tlsdorye@gmail.com>
2019-10-24 12:37:21 +0900

    채워야할 논리 완성 - CheckBox 구현, isCheck에 따라서 %버튼과 ^버튼 보이게 하기, 수식 결과 구현

--- app/src/main/java/com/example/calculator/MainActivity.java
+++ app/src/main/java/com/example/calculator/MainActivity.java
@@ -5,60 +5,104 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CheckBox;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import org.w3c.dom.Text;
 
 public class MainActivity extends AppCompatActivity {
+    private TextView mTextDisplay;
+    private CheckBox mBtnMode;
+    private StringBuilder mMathExpr;
+    private Button BtnDel;
+    private Button BtnPer;
+    private Button BtnPow;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        TextView mTextDisplay = (TextView)findViewById(R.id.textDisplay);
+        mTextDisplay = (TextView)findViewById(R.id.textDisplay);
+        mBtnMode = (CheckBox)findViewById(R.id.btnMode);
+        mMathExpr = new StringBuilder(256);
+        BtnDel = (Button)findViewById(R.id.btnDel);
+        BtnPer = (Button)findViewById(R.id.btnPer);
+        BtnPow = (Button)findViewById(R.id.btnPow);
+
+        BtnDel.setOnLongClickListener(new View.OnLongClickListener(){
+
+            @Override
+            public boolean onLongClick(View view) {
+                mMathExpr.delete(0, mMathExpr.length());
+                mTextDisplay.setText(mMathExpr);
+                return true;
+            }
+        });
+
     }
 
     public void mOnClick(View v) {
         switch (v.getId()) {
             case R.id.btnMode:
+                if(mBtnMode.isChecked()){
+                    BtnPer.setVisibility(View.VISIBLE);
+                    BtnPow.setVisibility(View.VISIBLE);
+                }
+                else {
+                    BtnPer.setVisibility(View.INVISIBLE);
+                    BtnPow.setVisibility(View.INVISIBLE);
+                }
                 break;
             case R.id.btn0:
-                break;
+                mMathExpr.append("0"); break;
             case R.id.btn1:
-                break;
+                mMathExpr.append("1"); break;
             case R.id.btn2:
-                break;
+                mMathExpr.append("2"); break;
             case R.id.btn3:
-                break;
+                mMathExpr.append("3"); break;
             case R.id.btn4:
-                break;
+                mMathExpr.append("4"); break;
             case R.id.btn5:
-                break;
+                mMathExpr.append("5"); break;
             case R.id.btn6:
-                break;
+                mMathExpr.append("6"); break;
             case R.id.btn7:
-                break;
+                mMathExpr.append("7"); break;
             case R.id.btn8:
-                break;
+                mMathExpr.append("8"); break;
             case R.id.btn9:
-                break;
+                mMathExpr.append("9"); break;
             case R.id.btnDot:
-                break;
+                mMathExpr.append("."); break;
             case R.id.btnAdd:
-                break;
+                mMathExpr.append("+"); break;
             case R.id.btnSub:
-                break;
+                mMathExpr.append("-"); break;
             case R.id.btnMul:
-                break;
+                mMathExpr.append("*"); break;
             case R.id.btnDiv:
+                mMathExpr.append("/"); break;
+            case R.id.btnPer:
+                mMathExpr.append("%"); break;
+            case R.id.btnPow:
+                mMathExpr.append("^"); break;
+            case R.id.btnEql:
+                if(mMathExpr.length() > 0)
+                    mMathExpr.deleteCharAt(mMathExpr.length() - 1);
                 break;
             case R.id.btnDel:
-                break;
-            case R.id.btnEql:
+                if(mMathExpr.length() == 0)
+                    return;
+                String result = NumCalc.calc(mMathExpr.toString());
+                if(result != null){
+                    mMathExpr.delete(0, mMathExpr.length());
+                    mMathExpr.append(result);
+                }
                 break;
         }
-        Toast.makeText(this, ((Button) v).getText(), Toast.LENGTH_SHORT).show();
+        mTextDisplay.setText(mMathExpr);
     }
 }
